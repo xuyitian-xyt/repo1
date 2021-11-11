edce4d26594a65d2bdf42355151f98a94fa3879d
xuyitian-xyt <63271281+xuyitian-xyt@users.noreply.github.com>
2021-11-11 17:01:21 +0800

    Update Test1.java

--- aProject/src/com/xyt/Test1.java
+++ aProject/src/com/xyt/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("xyt");
+		System.out.println("xyt2");
 		System.out.println("zhx");
 		System.out.println("xxt");
 		System.out.println("xzh");
